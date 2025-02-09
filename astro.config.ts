9a7ad4599be2124ec19a0630df458aa8bc4c63aa
tanmaypanda-14 <tanmay404103@outlook.com>
2025-02-10 01:29:08 +0530

    optimisations

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,8 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
@@ -17,6 +19,7 @@ export default defineConfig({
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
     }),
+    compress(),
   ],
   markdown: {
     remarkPlugins: [
@@ -40,7 +43,5 @@ export default defineConfig({
     },
   },
   scopedStyleStrategy: "where",
-  experimental: {
-    contentLayer: true,
-  },
+  experimental: { contentLayer: true },
 });
