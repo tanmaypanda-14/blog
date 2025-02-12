7d196a03332f8def1f3fbcbd0e49a3cfa8c77761
tanmaypanda-14 <tanmay404103@outlook.com>
2025-02-13 03:03:22 +0530

    feat: add Giscus comments integration to blog posts and update theme handling

--- public/toggle-theme.js
+++ public/toggle-theme.js
@@ -1,16 +1,10 @@
-const primaryColorScheme = ""; // "light" | "dark"
+const primaryColorScheme = "";
 
-// Get theme data from local storage
 const currentTheme = localStorage.getItem("theme");
 
 function getPreferTheme() {
-  // return theme value in local storage if it is set
   if (currentTheme) return currentTheme;
-
-  // return primary color scheme if it is set
   if (primaryColorScheme) return primaryColorScheme;
-
-  // return user device's prefer color scheme
   return window.matchMedia("(prefers-color-scheme: dark)").matches
     ? "dark"
     : "light";
@@ -21,40 +15,30 @@ let themeValue = getPreferTheme();
 function setPreference() {
   localStorage.setItem("theme", themeValue);
   reflectPreference();
+
+  window.dispatchEvent(new Event("themeChange"));
 }
 
 function reflectPreference() {
   document.firstElementChild.setAttribute("data-theme", themeValue);
-
   document.querySelector("#theme-btn")?.setAttribute("aria-label", themeValue);
 
-  // Get a reference to the body element
   const body = document.body;
-
-  // Check if the body element exists before using getComputedStyle
   if (body) {
-    // Get the computed styles for the body element
     const computedStyles = window.getComputedStyle(body);
-
-    // Get the background color property
     const bgColor = computedStyles.backgroundColor;
-
-    // Set the background color in <meta theme-color ... />
     document
       .querySelector("meta[name='theme-color']")
       ?.setAttribute("content", bgColor);
   }
 }
 
-// set early so no page flashes / CSS is made aware
 reflectPreference();
 
 window.onload = () => {
   function setThemeFeature() {
-    // set on load so screen readers can get the latest value on the button
     reflectPreference();
 
-    // now this script can find and listen for clicks on the control
     document.querySelector("#theme-btn")?.addEventListener("click", () => {
       themeValue = themeValue === "light" ? "dark" : "light";
       setPreference();
@@ -62,12 +46,9 @@ window.onload = () => {
   }
 
   setThemeFeature();
-
-  // Runs on view transitions navigation
   document.addEventListener("astro:after-swap", setThemeFeature);
 };
 
-// sync with system changes
 window
   .matchMedia("(prefers-color-scheme: dark)")
   .addEventListener("change", ({ matches: isDark }) => {
